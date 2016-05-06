f5eecd5992f5af6afe5b1089b2be49aabcfb7458
luis <luis@DESKTOP-U2LLTLC>
2016-05-06 12:13:16 +0200

    Add project files.

---
+++ GestionUsuarios/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="770ee9db-a73a-456f-b3c1-d1ed092b079f" description="SharePoint App Feature" featureId="770ee9db-a73a-456f-b3c1-d1ed092b079f" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="GestionUsuarios Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="6651bea8-1f49-4324-bfac-9f0efb2908be" />
+    <projectItemReference itemId="eeabc7bb-2ff9-4028-9f48-ceb3b9070598" />
+    <projectItemReference itemId="141adcab-4825-4266-a5d3-17ef0339dd32" />
+    <projectItemReference itemId="943f34b0-0713-4203-b12c-06086805bd34" />
+  </projectItems>
+</feature>
---
+++ GestionUsuarios/Scripts/App.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+function buscarUsuario() {
+    var txt = $("#txtNom").val();
+    var criterio = "AccountName:" + txt;
+    var query = new Microsoft.SharePoint.Client.
+                        Search.Query.KeywordQuery(context);
+    query.set_queryText(criterio);
+    query.set_sourceId("B09A7990-05EA-4AF9-81EF-EDFAB16C4E31");
+    var executor = new Microsoft.SharePoint.Client.Search.
+        Query.SearchExecutor(context);
+    var res = executor.executeQuery(query);
+
+    context.executeQueryAsync(function() {
+
+        if (res.m_value.ResultTables[0].ResultRows.length < 1) {
+            $("#res").html("Sin datos");
+            return;
+        }
+
+        var html = "<ul>";
+        $.each(res.m_value.ResultTables[0].ResultRows,
+            function (i, data) {
+
+                html += "<li>" + data.AccountName + " " +
+                    data.apodo + "</li>";
+            });
+        html += "</ul>";
+        $("#res").html(html);
+    },function() {
+        alert("KO");
+    });
+
+}
+$(document).ready(function() {
+    $("#btnBuscar").click(buscarUsuario);
+})
